f6f8d104bd15a596ad4b44be5e7714e26c08e6c9
brasbug <flywithme.top@gmail.com>
2017-10-26 22:03:00 +0800

    no message

---
+++ handler/routers.go
@@ -0,0 +1,49 @@
+package handler
+
+
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"strings"
+	"fmt"
+)
+
+
+var routers = map[string]gin.HandlerFunc{
+	"GET	 /":Index,
+}
+
+
+func RegisterRouters(r *gin.Engine)  {
+	for route, handler := range routers {
+		route = strings.TrimSpace(route)
+		fmt.Println(route)
+		method := "GET"
+		//空格
+		idx := strings.Index(route, " ")
+		//缩进
+		if idx == -1{
+			idx = strings.Index(route, "	")
+		}
+		if idx > -1 {
+			method = route[:idx]
+			route = strings.TrimSpace(route[idx+1:])
+		}
+		switch method {
+		case "POST":
+			r.POST(route, handler)
+		case "PUT":
+			r.PUT(route, handler)
+		case "HEAD":
+			r.HEAD(route, handler)
+		case "DELETE":
+			r.DELETE(route, handler)
+		case "OPTIONS":
+			r.OPTIONS(route, handler)
+		default:
+			r.GET(route, handler)
+		}
+	}
+
+}
