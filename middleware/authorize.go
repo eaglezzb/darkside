64e29566a7af9ba74fe213344000de3f2948bf51
brasbug <flywithme.top@gmail.com>
2017-11-03 22:46:38 +0800

    no message

---
+++ middleware/authorize.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	//"github.com/gin-gonic/gin"
+	//"regexp"
+	//"fmt"
+	//"github.com/brasbug/darkside/model"
+)
+
+//func Authorize()gin.HandlerFunc  {
+//	return func(c *gin.Context) {
+//		authHeader := c.Request.Header.Get("Authorization")
+//		r,_:= regexp.Compile("^Bearer (.+)$")
+//
+//		match := r.FindStringSubmatch(authHeader)
+//
+//		if len(match) == 0 {
+//			c.AbortWithStatus(401)
+//			return
+//		}
+//
+//		tokenString := match[1]
+//		if len(tokenString) == 0 {
+//			c.AbortWithStatus(401)
+//			return
+//		}
+//
+//		fmt.Println(tokenString)
+//		user := model.NewUser()
+//		user.Password = "abc"
+//		user.Uid ="1"
+//		user.Name = "a"
+//
+//		c.Set("user",user)
+//		c.Next()
+//
+//
+//	}
+//
+//
+//}
