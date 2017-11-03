977fd67839e7b911d97871024734792a20887eb1
brasbug <flywithme.top@gmail.com>
2017-11-04 01:07:03 +0800

    no message

--- model/model_user.go
+++ model/model_user.go
@@ -8,13 +8,15 @@ import (
 )
 
 type UserInfoModel struct {
-	Id  		int64 	`json:"id" form:"id"`
-	Uid  		string 	`json:"uid" form:"uid"`
-	Name	 	string 	`json:"name" form:"name"`
-	Password	string  `json:"password" form:"password"`
-	CreateTime	time.Time  `json:"created" form:"createTime"`
-	Sex 		string 	`json:"sex" form:"sex"`
-	UserId 		string 	`json:"userid" form:"userid"`
+	Id  		int64 		`json:"id" form:"id"`
+	Uid  		string 		`json:"uid" form:"uid"`
+	UserName	string 		`json:"username" form:"username"`
+	Password	string  	`json:"password" form:"password"`
+	CreateTime	time.Time  	`json:"createtime" form:"createtime"`
+	UpdateTime	time.Time  	`json:"updatetime" form:"updatetime"`
+	Sex 		int 		`json:"sex" form:"sex"`     //0默认未设置 1男，2女
+	UserId 		string 		`json:"userid" form:"userid"`
+	DepartName	string		`json:"departname" form:"departname"`
 }
 
 func NewUser() UserInfoModel {
@@ -22,22 +24,20 @@ func NewUser() UserInfoModel {
 }
 
 func (user *UserInfoModel)ToString()(desc string)  {
-	desc = "name:"+user.Name + " " + "Uid:"+user.Uid
+	desc = "name:"+user.UserName + " " + "Uid:"+user.Uid
 	return desc
 }
 
 func (user *UserInfoModel)InsertUser(){
 	db := db.DBConf()
+	fmt.Println(user.UserName)
 
-	stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,createtime=?")
+	stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,createtime=?,password=?,sex=?")
 	checkErr(err)
 
-	res, err := stmt.Exec("码农", "技术部", "2017-11-06")
+	_, err = stmt.Exec(user.UserName, user.DepartName, time.Now(),user.Password,user.Sex)
 	checkErr(err)
 
-	id, err := res.LastInsertId()
-	checkErr(err)
-	fmt.Println(id)
 
 }
 
