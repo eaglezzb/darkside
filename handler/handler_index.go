abc3e03edac373b648eaa7319966a555b22068d3
brasbug <flywithme.top@gmail.com>
2017-11-03 22:45:45 +0800

    no message

--- handler/handler_index.go
+++ handler/handler_index.go
@@ -2,13 +2,16 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
-	"fmt"
+	"github.com/brasbug/darkside/test"
 )
+
 
 
 //golang中根据首字母的大小写来确定可以访问的权限。无论是方法名、常量、变量名还是结构体的名称，如果首字母大写，则可以被其他的包访问；如果首字母小写，则只能在本包中使用
 func Index(ctx *gin.Context)  {
-	fmt.Println("ctx====ctx",ctx)
+
+	test.DBTest()
+
 	ctx.JSON(200,gin.H{
 		"errno":"0",
 		"msg":"Index.html",
