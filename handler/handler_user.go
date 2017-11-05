5004503ef6e352b96ae2ca327f3b9c83eb12377b
brasbug <flywithme.top@gmail.com>
2017-11-05 15:39:10 +0800

    api 获取userInfo

--- handler/handler_user.go
+++ handler/handler_user.go
@@ -1,11 +1,38 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	m "github.com/brasbug/darkside/model"
+	//"encoding/json"
+	log "github.com/brasbug/log4go"
+	"net/http"
+	"strconv"
+)
 
-func doRegisterHandler(ctx *gin.Context )  {
-
-
+func RegisterHandler(ctx *gin.Context )  {
 
 
 
 }
+
+//user/:uid
+func GetUserInfo(c *gin.Context)  {
+	uid ,_ := strconv.ParseInt(c.Param("uid"),10,64)
+	user,err := m.FindUserFromDB(uid)
+	if err != nil{
+		log.Error(err.Error(),err)
+		c.JSON(http.StatusOK,gin.H{
+			"userinfo":nil,
+			"code":404,
+			"message":"未查询到该用户信息",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK,gin.H{
+		"userinfo":user,
+		"code":http.StatusOK,
+		"message":"",
+	})
+}
+
