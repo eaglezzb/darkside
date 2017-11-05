85418a67deb4d688740dca6e63f00a0a8710c70c
brasbug <flywithme.top@gmail.com>
2017-11-05 22:45:18 +0800

    正则更新

--- handler/handler_user.go
+++ handler/handler_user.go
@@ -8,13 +8,12 @@ import (
 	"net/http"
 	"strconv"
 	"regexp"
-	"fmt"
 )
 
 func RegisterHandler(c *gin.Context )  {
 	user := m.NewUser()
 	user.UserName = c.PostForm("username")
-	if valideUserName(user.UserName) {
+	if !valideUserName(user.UserName) {
 		c.JSON(http.StatusOK,gin.H{
 			"code":http.StatusBadRequest,
 			"message":"用户名不复合要求",
@@ -22,7 +21,7 @@ func RegisterHandler(c *gin.Context )  {
 		return
 	}
 	user.Password = c.PostForm("password")
-	if valideUserName(user.Password) {
+	if !validePassword(user.Password) {
 		c.JSON(http.StatusOK,gin.H{
 			"code":http.StatusBadRequest,
 			"message":"密码不符合要求",
@@ -31,8 +30,19 @@ func RegisterHandler(c *gin.Context )  {
 	}
 	user.Sex,_ = strconv.Atoi(c.PostForm("sex"))
 	user.DepartName = c.PostForm("departname")
-	user.PhonePrefix ,_ = strconv.ParseInt(c.PostForm("phoneprefix"),10,6)
-	user.Phone ,_ = strconv.ParseInt(c.PostForm("phone"),10,11)
+	phonePrefixstr := c.PostForm("phoneprefix")
+	user.PhonePrefix ,_ = strconv.ParseInt(phonePrefixstr,10,64)
+
+	phonestr := c.PostForm("phone")
+
+	if !validePhone(phonestr) {
+		c.JSON(http.StatusOK,gin.H{
+			"code":http.StatusBadRequest,
+			"message":"手机号不符合要求",
+		})
+		return
+	}
+	user.Phone ,_ = strconv.ParseInt(phonestr,10,64)
 
 	err := user.InsertUser()
 	if err != nil {
@@ -52,21 +62,24 @@ func RegisterHandler(c *gin.Context )  {
 }
 
 func valideUserName(name string)bool  {
-	reg := regexp.MustCompile("/^[a-zA-Z0-9_-]{4,16}$/")
+	reg := regexp.MustCompile("^[a-zA-Z0-9_-]{4,16}$")
 	return reg.MatchString(name)
 }
 
 func validePassword(name string)bool  {
-	reg := regexp.MustCompile("/^[a-zA-Z0-9_-]{4,16}$/")
+	reg := regexp.MustCompile("^[a-zA-Z0-9_-]{4,16}$")
 	return reg.MatchString(name)
 }
 
+func validePhone(name string)bool  {
+	reg := regexp.MustCompile("^[1-9][0-9]{4,13}$")
+	return reg.MatchString(name)
+}
 
 //user/:uid
 func GetUserInfo(c *gin.Context)  {
 	uid ,_ := strconv.ParseInt(c.Param("uid"),10,64)
 	user,err := m.FindUserFromDB(uid)
-	fmt.Println(user)
 	if err != nil{
 		log.Error(err.Error(),err)
 		c.JSON(http.StatusOK,gin.H{
