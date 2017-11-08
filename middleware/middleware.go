7a4dda71a3593d3e4963a7dc196432042f4e6922
flywithbug <flywithme.top@gmail.com>
2017-11-08 23:55:06 +0800

    no message

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -4,9 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	log "github.com/flywithbug/log4go"
 	"sync"
-	"net/http"
-	"net"
-	"encoding/binary"
+	"github.com/flywithbug/utils"
 )
 
 var wg sync.WaitGroup
@@ -19,40 +17,7 @@ func Middleware(c *gin.Context)  {
 
 func wirteLog(c *gin.Context)  {
 	c.Request.ParseForm()
-	log.Info("Form:%s,,Path:%s%s",c.Request.Form,RemoteIp(c.Request),c.Request.RequestURI)
+	log.Info("Form:%s,,Path:%s%s",c.Request.Form,utils.RemoteIp(c.Request),c.Request.RequestURI)
 	wg.Done()
 }
 
-const (
-	XForwardedFor = "X-Forwarded-For"
-	XRealIP       = "X-Real-IP"
-)
-
-
-func RemoteIp(req *http.Request) string {
-	remoteAddr := req.RemoteAddr
-	if ip := req.Header.Get(XRealIP); ip != "" {
-		remoteAddr = ip
-	} else if ip = req.Header.Get(XForwardedFor); ip != "" {
-		remoteAddr = ip
-	} else {
-		remoteAddr, _, _ = net.SplitHostPort(remoteAddr)
-	}
-
-	if remoteAddr == "::1" {
-		remoteAddr = "127.0.0.1"
-	}
-
-	return remoteAddr
-}
-
-
-// Ip2long 将 IPv4 字符串形式转为 uint32
-func Ip2long(ipstr string) uint32 {
-	ip := net.ParseIP(ipstr)
-	if ip == nil {
-		return 0
-	}
-	ip = ip.To4()
-	return binary.BigEndian.Uint32(ip)
-}
