f78463f9608abd9f1b1e386aed35dc0f3713795e
flywithbug <flywithme.top@gmail.com>
2017-11-09 17:01:14 +0800

    use json encode

--- handler/handler_index.go
+++ handler/handler_index.go
@@ -10,7 +10,7 @@ import (
 //golang中根据首字母的大小写来确定可以访问的权限。无论是方法名、常量、变量名还是结构体的名称，如果首字母大写，则可以被其他的包访问；如果首字母小写，则只能在本包中使用
 func Index(ctx *gin.Context)  {
 
-	test.DBTest()
+	test.Test()
 
 	ctx.JSON(200,gin.H{
 		"errno":"0",
