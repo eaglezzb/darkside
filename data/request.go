72766ae574b3986001113033f018539a775bfc14
flywithbug <flywithme.top@gmail.com>
2017-11-12 00:46:03 +0800

    req

--- data/request.go
+++ data/request.go
@@ -1,1 +1,31 @@
 package data
+
+//import (
+//	"net/http"
+//	log "github.com/flywithbug/log4go"
+//
+//)
+//
+//
+//
+//func GetRequest(urls string)(interface{},error)  {
+//
+//
+//}
+//
+//func PostRequest(urls string)()  {
+//
+//
+//}
+//
+//
+//
+//
+//
+//func checkErr(err error) {
+//	if err != nil {
+//		log.Warn(err.Error())
+//	}
+//}
+
+
