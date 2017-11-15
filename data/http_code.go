afd2e54a0494695f8b350198283d0ee67e6104d4
flywithbug <flywithme.top@gmail.com>
2017-11-15 22:53:52 +0800

    custom request code

---
+++ data/http_code.go
@@ -0,0 +1,28 @@
+package data
+
+
+const (
+
+	//CallBackCode
+
+	ErrcodeRequestSystemError  = 1001   //系统错误
+
+
+	ErrcodeRequestParamsInvalid   = 1101   //请求参数无法正常获取
+	ErrCodeRequestInvalidPara    = 1102   //某参数校验未通过
+	ErrCodeRequestVerifyouttime   = 1103   //验证超时
+
+
+
+
+	ErrCodeRequestToorequently    = 1211  //请求发送太频繁
+
+
+
+
+
+
+
+
+
+)
