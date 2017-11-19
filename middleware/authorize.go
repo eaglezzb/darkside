0a402189d1a4b178c47ccddff05344cd52c33f83
flywithbug <flywithme.top@gmail.com>
2017-11-19 23:53:35 +0800

    验证中间件 TODO!!!

--- middleware/authorize.go
+++ middleware/authorize.go
@@ -1,36 +1,33 @@
 package middleware
 
-//import (
-//	"github.com/gin-gonic/gin"
-//	"regexp"
-//	"fmt"
-//)
-//
-//
-//
-//func Authorize() gin.HandlerFunc {
-//	return func(c *gin.Context) {
-//		authHeader := c.Request.Header.Get("Authorization")
-//		fmt.Println("authHeader", authHeader)
-//		r, _ := regexp.Compile("^Bearer (.+)$")
-//
-//		match := r.FindStringSubmatch(authHeader)
-//
-//		if len(match) == 0 {
-//			c.AbortWithStatus(401)
-//			return
-//		}
-//		tokenString := match[1]
-//
-//		if len(tokenString) == 0 {
-//			c.AbortWithStatus(401)
-//			return
-//		}
-//
-//		user := "tokenString"
-//
-//		c.Set("user", user)
-//		c.Set("userID", user)
-//		c.Next()
-//	}
-//}
+import (
+	"github.com/gin-gonic/gin"
+	"regexp"
+	"fmt"
+)
+
+
+
+func Authorize() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.Request.Header.Get("authtoken")
+		fmt.Println("authHeader", authHeader)
+		r, _ := regexp.Compile("^Bearer (.+)$")
+
+		match := r.FindStringSubmatch(authHeader)
+
+		if len(match) == 0 {
+			c.AbortWithStatus(401)
+			return
+		}
+		tokenString := match[1]
+
+		if len(tokenString) == 0 {
+			c.AbortWithStatus(401)
+			return
+		}
+
+		c.Next()
+		return
+	}
+}
