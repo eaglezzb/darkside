e99deb1951caea2a536eeda8002239206ab29ca2
flywithbug <flywithme.top@gmail.com>
2017-11-20 23:44:49 +0800

    数据库插入用户关系api

--- model/model_connect.go
+++ model/model_connect.go
@@ -1,1 +1,35 @@
 package model
+
+import (
+	"github.com/flywithbug/darkside/db"
+	log "github.com/flywithbug/log4go"
+
+)
+
+type UserConnectModel struct {
+	Uid  		int64 		`json:"uid,omitempty" form:"uid,omitempty"`
+	Userid_1 	float32		`json:"userid_1,omitempty" form:"userid_1,omitempty"`
+	Userid_2	float32  	`json:"userid_2,omitempty" form:"userid_2,omitempty"`
+}
+
+func (ucnn *UserConnectModel)AddConnect()error  {
+	db := db.DBConf()
+	stmt, err := db.Prepare("INSERT user_friend_connect SET userid_1=?,userid_2=?")
+	if err != nil {
+		log.Warn(err.Error())
+		return err
+	}
+	if ucnn.Userid_1 > ucnn.Userid_2 {
+		ucnn.Userid_1,ucnn.Userid_2 = ucnn.Userid_2,ucnn.Userid_1
+	}
+	_, err = stmt.Exec(ucnn.Userid_1,ucnn.Userid_2)
+	if err != nil {
+		log.Warn(err.Error())
+	}
+	return err
+}
+
+
+
+
+
