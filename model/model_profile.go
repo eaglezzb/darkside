5c1053d86c3f36e646209a0d17cc5125cd7fdc71
flywithbug <flywithme.top@gmail.com>
2017-11-22 21:25:40 +0800

    新增用户信息表

--- model/model_profile.go
+++ model/model_profile.go
@@ -1,1 +1,36 @@
 package model
+import (
+	"github.com/flywithbug/darkside/db"
+	log "github.com/flywithbug/log4go"
+	"time"
+)
+
+
+
+type UserProfileModel struct {
+	Uid  		int64 		`json:"uid,omitempty" form:"uid,omitempty"`
+	Avatar		string 		`json:"avatar,omitempty" form:"avatar,omitempty"`
+	UserId 		string 		`json:"userid,omitempty" form:"userid,omitempty"`
+	UpdateTime	int64  		`json:"updatetime,omitempty" form:"updatetime,omitempty"`
+}
+
+
+func (profile *UserProfileModel)UpdateUserProfile()error {
+	db := db.DBConf()
+
+	stmt, err := db.Prepare("INSERT profile SET uid =?,userid=?,avatar=?, updatetime=? on duplicate key update uid =?,userid=?,avatar=?, updatetime=?")
+	if err != nil{
+		log.Warn(err.Error())
+		return err
+	}
+	profile.UpdateTime = time.Now().Unix()
+	_, err = stmt.Exec(profile.Uid,profile.UserId,profile.Avatar,profile.UpdateTime,profile.Uid,profile.UserId,profile.Avatar,profile.UpdateTime)
+	if err != nil {
+		log.Warn(err.Error())
+		return err
+	}
+	return err
+}
+
+
+
